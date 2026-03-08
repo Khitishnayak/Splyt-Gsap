bd901ea0c55dd7c4bbfeecd0599a95e403c8188d
Khitishnayak <kknayak049@gmail.com>
2026-03-09 00:35:01 +0530

    removed markers

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -35,7 +35,7 @@ const TestimonialSection = () => {
         end: "200% top",
         scrub: 1.5,
         pin: true,
-        markers : true,
+        
       }
     });
     pinTl.from(".vd-card", {
